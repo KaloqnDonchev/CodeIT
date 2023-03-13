bac719d606fd5e88317e71d183f059523587b2a7
Kaloyan <kaloqndonchev99@gmail.com>
2023-03-13 23:14:39 +0200

    formatting changes

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ app.get('/countries', (req, res) => {
         result.rows.forEach(row => {
             countries.push(row.country);
         });
-        res.json(countries);
+        res.status(200).json(countries);
     });
 });
 
 app.post('/register', async (req, res) => {
-    ({ email, username, realname, password, birthdate } = req.body);
+    const { email, username, realname, password, birthdate } = req.body;
     if (password.length < 6) return res.json('Invalid password');
     if (!isEmail(email)) return res.json('Invalid email');
     const cryptedPassword = await bcrypt.hash(password, 10);
@@ -53,14 +53,14 @@ app.post('/register', async (req, res) => {
             console.log(err.message);
             return;
         }
-        res.json(userDetails);
+        res.status(200).json(userDetails);
         res.sendFile(__dirname + '/public/home-page.html');
     });
-    client.end;
+    client.end();
 });
 
 app.post('/login', async (req, result) => {
-    ({ emailOrUsername, password } = req.body);
+    const { emailOrUsername, password } = req.body;
 
     const selectQuery = `SELECT email, username, password FROM users WHERE email='${emailOrUsername}' OR username='${emailOrUsername}'`;
 
@@ -72,12 +72,12 @@ app.post('/login', async (req, result) => {
         const cryptedPass = queryResult.rows[0].password;
 
         if (await bcrypt.compare(password, cryptedPass)) {
-            result.json(userDetails);
+            result.status(200).json(userDetails);
         } else {
-            result.json('Incorrect password')
+            result.status(400).json('Incorrect password')
         }
     } catch (err) {
-        throw new Error('Failed to do');
+        throw new Error('Failed to find user and login');
     }
 
     client.end();
